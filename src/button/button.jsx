fb9dd1305f33291e6ec13e1c2ec4f49fd682dd0c
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2024-03-05 16:35:58 -0500

    Delete src/button directory

--- src/button/button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./button.css";
-
-const Buttonn = ({ onClick, children }) => {
-  return <button onClick={onClick} className="button">{children}</button>;
-};
-
-export default Buttonn;
